69806ef43d120bd58db4ad9dfc74d13d7ac7faa5
Kaguya-19 <2561525405@qq.com>
2022-04-01 22:37:30 +0800

    map

--- GStrategy2022-frontend/Assets/Scripts/HexGrid.cs
+++ GStrategy2022-frontend/Assets/Scripts/HexGrid.cs
@@ -1,97 +1,6 @@
 using UnityEngine;
 using System.Collections;
-enum Types { nor,bar,res };//0:normal 1:barrier 2:resource TODO:缩圈,更改资源
-public class MapUnit : MonoBehaviour 
-{
-    private int x = 0;
-    private int y = 0;
-    private int z = 0;
-    private GameObject cell;
-    private GameObject resource;
-    private int type = (int)Types.nor; 
-    private int resource_num = 0;
-    
-    public MapUnit()
-    {
-
-    }
-    public void init(int t_x,int t_z,float length, int t_type, GameObject hexPrefab,GameObject resPrefab, int t_res_num = 0)
-    {
-        x = t_x - t_z/2;
-        z = t_z;
-        y = -x - z;
-        type = t_type;
-        if (type == (int)Types.res)
-            resource_num = t_res_num;
-
-        //生成地图块
-        float innerRadius = 1.73205081f * length * 0.5f;
-        float outerRadius = length;
-        Vector3 position;
-        position.x = (t_x + (t_z % 2) * 0.5f) * (innerRadius * 2f);
-        position.y = 0f;
-        position.z = t_z * (outerRadius * 1.5f);
-
-        GameObject cell = Instantiate<GameObject>(hexPrefab);
-        cell.transform.SetParent(transform, false);
-        cell.transform.localPosition = position;
-
-
-        switch(type)
-        {
-            case 1://障碍
-                {
-                    Vector3 pos_obs = cell.transform.localPosition;
-                    pos_obs.y = 100f;
-                    cell.transform.localPosition = pos_obs;
-                    Vector3 scale_obs = cell.transform.localScale;
-                    scale_obs.y = 1000f;
-                    cell.transform.localScale = scale_obs;
-                    break;
-                }
-            case 2://资源 TODO：根据资源数修改模型
-                {
-                    Vector3 pos_res = Vector3.zero;
-                    pos_res.y = 1f;
-                    GameObject resource = Instantiate<GameObject>(resPrefab);
-                    resource.transform.SetParent(cell.transform, false);
-                    resource.transform.localPosition = pos_res;
-                    break;
-                }
-            default:
-                break;
-        }
-    }
-
-    public int[] GetHexCoor() //六边形坐标位置
-    {
-        int[] res = { x, y, z };  
-        return res;
-    }
-    
-    public Vector3 GetGloPos() //全局坐标
-    {
-        return transform.TransformPoint(cell.transform.position);
-    }
-    public Vector3 GetResPos() //相对坐标
-    {
-        return cell.transform.position;
-    }
-    public GameObject GetCell()//返回地图块
-    {
-        return cell;
-    }
-
-    /*public GameObject GetMonster()
-    {
-        return monster;
-    }*/
-
-    public GameObject GetRes()//返回资源
-    {
-        return resource;
-    }
-}
+using UnityEngine.UI;
 
 public class HexGrid : MonoBehaviour //TODO:与后端文件联动
 {
@@ -102,10 +11,15 @@ public class HexGrid : MonoBehaviour //TODO:与后端文件联动
     public GameObject hexPrefab;
     //public GameObject monsterPrefab;
     public GameObject resPrefab;
+    public Text textPrefab;
 
-    ArrayList units = new ArrayList();
+    GameObject[] units;
 
     void Awake()
+    {
+    }
+
+    void init()
     {
         if (width % 2 == 1)
         {
@@ -114,15 +28,18 @@ public class HexGrid : MonoBehaviour //TODO:与后端文件联动
         if (width <= 2)
             return;
         w = 2 * width;
+        units = new GameObject[w * w];
 
-
-        for (int i = 0; i < w; i++)
-        {
-            for (int j = 0; j < w; j++)
-            {
-                units.Add(gameObject.AddComponent<MapUnit>());
-            }
-        }
+        //for (int i = 0; i < w; i++)
+        //{
+        //    for (int j = 0; j < w; j++)
+        //    {
+        //        //GameObject tmp_gO = new GameObject();
+        //        //tmp_gO.AddComponent<MapUnit>();
+        //        //units.Add(tmp_gO);
+        //        //units.Add(gameObject.AddComponent<MapUnit>());
+        //    }
+        //}
 
         for (int z = 0; z < w - 1; z++)
         {
@@ -131,14 +48,18 @@ public class HexGrid : MonoBehaviour //TODO:与后端文件联动
             int tmp_e = w - 2 - width_tmp  / 2;
             for (int x = tmp_s; x <= tmp_e; x++)
             {
-                ((MapUnit)units[x + w * z]).init(x, z, length, (int)Types.nor, hexPrefab, resPrefab);
+                GameObject tmp_hex = units[z * w + x] = createHex(x, z, length);
+                tmp_hex.AddComponent<MapUnit>();
+                tmp_hex.GetComponent<MapUnit>().init(x, z,w, (int)Types.nor, tmp_hex, resPrefab,0,textPrefab);
             }
         }
     }
 
     public int[] hexToNormal(int h_x, int h_z)
     {
-        int[] normal = { h_x + h_z / 2, h_z };
+        int t_z = h_z + (2 * width - 1) / 2;
+        int t_x = h_x + width / 2 + t_z / 2;
+        int[] normal = { t_x, t_z };
         return normal;
     }
 
@@ -147,7 +68,32 @@ public class HexGrid : MonoBehaviour //TODO:与后端文件联动
         int[] n = hexToNormal(h_x, h_z);
         int n_x = n[0];
         int n_z = n[1];
-        return (MapUnit)units[n_z * w + n_x];
+        return (units[n_z * w + n_x].GetComponent<MapUnit>());
     }
-    
+
+    public Vector3 GetUnitTransform(int h_x, int h_z)
+    {
+        int[] n = hexToNormal(h_x, h_z);
+        int n_x = n[0];
+        int n_z = n[1];
+        GameObject unit = units[n_z * w + n_x];
+        if (unit == null) Debug.Log("ERROR");
+        Vector3 point = new Vector3(unit.transform.position.x, 0, unit.transform.position.z);
+        return point;
+    }
+    public GameObject createHex(int t_x, int t_z, float length)
+    {
+        //生成地图块
+        float innerRadius = 1.73205081f * length * 0.5f;
+        float outerRadius = length;
+        Vector3 position;
+        position.x = (t_x + (t_z % 2) * 0.5f) * (innerRadius * 2f);
+        position.y = 0f;
+        position.z = t_z * (outerRadius * 1.5f);
+        GameObject cell = Instantiate<GameObject>(hexPrefab);
+        cell.transform.SetParent(transform, false);
+        cell.transform.localPosition = position;
+        return cell;
+    }
+
 }
